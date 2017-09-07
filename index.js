772018462c90c2fef67a138a3905f1cf7c385d51
nodkz <pavel.chertorogov@gmail.com>
2017-09-07 18:33:30 +0600

    feat: Add support for babel-plugin-relay@1.3.0
    Related changes in upstream:
    https://github.com/facebook/relay/commit/8fc94eef47f12d62136345759c9a5dd8f49cb710

--- index.js
+++ index.js
@@ -57,10 +57,72 @@ function initBabelRelayPlugin(pluginOptions, babel, ref) {
     log('Relay.QL will not be transformed, cause `schema.data` is empty.');
     ref.babelRelayPlugin = {
       visitor: {
+        Program: function () {},
         TaggedTemplateExpression: function () {},
       },
     };
   }
+}
+
+function initProxy(babel, state) {
+  const ref = {};
+  const pluginOptions = state.opts || {};
+
+  if (pluginOptions.schemaJsonFilepath) {
+    log(
+      '[transform-relay-hot] Please rename `schemaJsonFilepath` option in .babelrc:'
+      + '\n   {'
+      + '\n     "plugins": ['
+      + '\n       ["transform-relay-hot", {'
+      + '\n 🛑 from:  "schemaJsonFilepath": "./pathToSchema.json",'
+      + '\n 👌   to:  "schema": "./pathToSchema.json",'
+      + '\n       }]'
+      + '\n     ]'
+      + '\n   }'
+    );
+    pluginOptions.schema = pluginOptions.schemaJsonFilepath;
+  }
+
+  if (!pluginOptions.schema || pluginOptions.schema === '') {
+    log(
+      '[transform-relay-hot] You should provide `schema` option in .babelrc:'
+      + '\n   {'
+      + '\n     "plugins": ['
+      + '\n       ["transform-relay-hot", {'
+      + '\n         "schema": "./pathToSchema.json",'
+      + '\n         "watchInterval": 2000'
+      + '\n       }]'
+      + '\n     ]'
+      + '\n   }'
+    );
+  }
+
+  // HACK obtain/update babelRelayPlugin by reference
+  initBabelRelayPlugin(pluginOptions, babel, ref);
+
+  const watchInterval = pluginOptions && pluginOptions.watchInterval
+    ? pluginOptions.watchInterval
+    : 2000;
+  if (watchInterval > 0 && pluginOptions.schema) {
+    const reinitBabelRelayPlugin = () => {
+      log('Re-init babel-plugin-relay');
+      // decache all babel-plugin-relay modules
+      Object.keys(require.cache).forEach((key) => {
+        if (key.indexOf('babel-plugin-relay') > 0) {
+          delete require.cache[key];
+        }
+      });
+      Object.keys(module.constructor._pathCache).forEach((key) => {
+        if (key.indexOf('babel-plugin-relay') > 0) {
+          delete module.constructor._pathCache[key];
+        }
+      });
+      initBabelRelayPlugin(pluginOptions, babel, ref);
+    };
+    watcherFn(pluginOptions.schema, watchInterval, reinitBabelRelayPlugin);
+  }
+
+  return ref;
 }
 
 
@@ -73,72 +135,19 @@ module.exports = function (babel) {
        * Extract the module name from `@providesModule`.
        */
       Program(path, state) {
-        // HACK ONLY ONCE obtain plugin configs form .babelrc and init BabelRelayPlugin
-        if (!ref) {
-          ref = {};
-          const pluginOptions = state.opts || {};
+        // HACK ONLY ONCE obtain plugin configs form .babelrc and init our proxy
+        if (!ref) ref = initProxy(babel, state);
 
-          if (pluginOptions.schemaJsonFilepath) {
-            log(
-              '[transform-relay-hot] Please rename `schemaJsonFilepath` option in .babelrc:'
-              + '\n   {'
-              + '\n     "plugins": ['
-              + '\n       ["transform-relay-hot", {'
-              + '\n 🛑 from:  "schemaJsonFilepath": "./pathToSchema.json",'
-              + '\n 👌   to:  "schema": "./pathToSchema.json",'
-              + '\n       }]'
-              + '\n     ]'
-              + '\n   }'
-            );
-            pluginOptions.schema = pluginOptions.schemaJsonFilepath;
-          }
-
-          if (!pluginOptions.schema || pluginOptions.schema === '') {
-            log(
-              '[transform-relay-hot] You should provide `schema` option in .babelrc:'
-              + '\n   {'
-              + '\n     "plugins": ['
-              + '\n       ["transform-relay-hot", {'
-              + '\n         "schema": "./pathToSchema.json",'
-              + '\n         "watchInterval": 2000'
-              + '\n       }]'
-              + '\n     ]'
-              + '\n   }'
-            );
-          }
-
-          // HACK obtain/update babelRelayPlugin by reference
-          initBabelRelayPlugin(pluginOptions, babel, ref);
-
-          const watchInterval = pluginOptions && pluginOptions.watchInterval
-            ? pluginOptions.watchInterval
-            : 2000;
-          if (watchInterval > 0 && pluginOptions.schema) {
-            const reinitBabelRelayPlugin = () => {
-              log('Re-init babel-plugin-relay');
-              // decache all babel-plugin-relay modules
-              Object.keys(require.cache).forEach((key) => {
-                if (key.indexOf('babel-plugin-relay') > 0) {
-                  delete require.cache[key];
-                }
-              });
-              Object.keys(module.constructor._pathCache).forEach((key) => {
-                if (key.indexOf('babel-plugin-relay') > 0) {
-                  delete module.constructor._pathCache[key];
-                }
-              });
-              initBabelRelayPlugin(pluginOptions, babel, ref);
-            };
-            watcherFn(pluginOptions.schema, watchInterval, reinitBabelRelayPlugin);
-          }
-        }
+        const fn = ref.babelRelayPlugin.visitor.Program;
+        if (fn) fn(path, state);
       },
 
       /**
        * Transform Relay.QL`...`.
        */
       TaggedTemplateExpression(path, state) {
-        ref.babelRelayPlugin.visitor.TaggedTemplateExpression(path, state);
+        const fn = ref.babelRelayPlugin.visitor.TaggedTemplateExpression;
+        if (fn) fn(path, state);
       },
     },
   };
